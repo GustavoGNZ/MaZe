8ff1063ac43828af192285c774051b93ff5a9e22
Gustavo Ganaza <gustavo.ganaza@hotmail.com>
2025-06-10 15:57:25 -0300

    teste

--- maze.c
+++ maze.c
@@ -6,7 +6,7 @@ int main (){
 
     // bitboard x = 128ULL; // Inicializa o bitboard com o primeiro bit definido
     // printf("Bitboard: %llu\n", x); // Imprime o valor do bitboard
-   
+    printf("Hello, World!\n"); // Imprime uma mensagem de saudação
    
     return 0;
 }
